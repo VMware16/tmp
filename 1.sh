7a3e98257e7d885e5672623b5fa3b38eef337d6c
VMware16 <100504412+VMware16@users.noreply.github.com>
2022-04-12 22:13:07 +0800

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+My most exciting day
+There are a lot of days in my life. But that exciting day makes me unforgettable.
+At first, I was chosen to take part in an online international water rocket activity.After that, to get a better score,I made several vater rockets and practiced launching them for a few days.Finally,it was time for the competition.
+In the morning,I came to school with my water rocket.Then the activity began.First,I watched others students lanch their own water rockets.Next,it’s my turn.I took out my water rocket and connected it to the launcher.I pressed the button and my water rocket flew away.It landed on the right place!How exciting it was!At last,I was excited to hear that our country won the best group award.I was very excited.
+That day was so exciting that I won’t forget it forever.How important it is to have a full preparing before doing things!
